65da942e339fc123d626eabbb037221f35422668
giorjorio <105600836+giorjorio@users.noreply.github.com>
2023-09-26 16:55:06 +0300

    Pickup class set up

--- Source/Blaster/Pickups/Pickup.h
+++ Source/Blaster/Pickups/Pickup.h
@@ -6,21 +6,45 @@
 #include "GameFramework/Actor.h"
 #include "Pickup.generated.h"
 
+
+class USphereComponent;
+class USoundCue;
+
 UCLASS()
 class BLASTER_API APickup : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	APickup();
+	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	USphereComponent* OverlapSphere;
+
+	UPROPERTY(EditAnywhere)
+	USoundCue* PickupSound;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* PickupMesh;
+
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	
 
 };
