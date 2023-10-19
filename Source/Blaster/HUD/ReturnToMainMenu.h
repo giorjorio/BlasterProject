9d9f7661cec370c7fe81dfbce97ed11037be75ce
giorjorio <105600836+giorjorio@users.noreply.github.com>
2023-10-19 20:34:08 +0300

    Return to main menu widget created

---
+++ Source/Blaster/HUD/ReturnToMainMenu.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "ReturnToMainMenu.generated.h"
+
+
+
+class APlayerController;
+class UButton;
+class UMultiplayerSessionsSubsystem;
+
+
+
+UCLASS()
+class BLASTER_API UReturnToMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void MenuSetup();
+	void MenuTearDown();
+
+protected:
+	virtual bool Initialize() override;
+
+	UFUNCTION()
+	void OnDestroySession(bool bWasSuccessfull);
+
+
+private:
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ReturnButton;
+	
+	UFUNCTION()
+	void ReturnButtonClicked();
+	
+	UPROPERTY()
+	UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem;
+
+
+	UPROPERTY()
+	APlayerController* PlayerController;
+};
